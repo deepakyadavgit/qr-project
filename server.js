5107fdb958f698ab4dac464f484c45457b214da9
Gyanaranjan Sahoo <gyanaranjansahoo509@gmail.com>
2024-06-05 03:36:46 +0530

    cors changes

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json({ limit: "16kb" }));
 // to encode the urls and limit the query size after request
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
